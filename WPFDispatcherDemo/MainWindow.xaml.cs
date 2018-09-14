b4c0e718ede69b744a4c3cb26effcb5a01544dc1
Mayur Banta <mayurbanta@gmail.com>
2018-09-14 09:23:41 +0530

    Add project files.

---
+++ WPFDispatcherDemo/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Windows;
+
+namespace WPFDispatcherDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(SetText));
+            t.Start();
+        }
+
+        private void SetText()
+        {
+            this.Dispatcher.Invoke(() => { myTextBlock.Text = "Set by Dispatcher"; });
+        }
+    }
+}
